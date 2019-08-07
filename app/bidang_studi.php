ef008306503ed92e2c239532190f16bd8e1e0a5c
ArifRidhwan <aridridhwan68@gmail.com>
2019-08-07 17:16:48 +0700

    add relations

---
+++ app/bidang_studi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class bidang_studi extends Model
+{
+    
+}
---
+++ app/kompetensi_keahlian.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class kompetensi_keahlian extends Model
+{
+    //
+}
