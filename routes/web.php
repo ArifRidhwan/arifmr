c4afae192ff396fc6c570f69924402f8963008f2
ArifRidhwan <arifridhwan68@gmail.com>
2019-08-13 08:29:17 +0700

    heroku auth

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 route::group(['prefix'=>'backend','middleware'=>['auth','role::seperadmin']],function(){
     route::resource('user','UserController');    
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
